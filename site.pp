ebd987f70c4c1459aec437393c77d0e9a8370e8a
sandeep3162 <devops.sandeep@gmail.com>
2016-08-12 09:57:46 -0400

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,4 +1,23 @@
-node {agent*} 
-{
-include nginx
+
+node '*' {  
+  class { 'apache':    
+    default_mods => false, 
+  }  
+
+  include apache::mod::ssl  
+
+  apache::vhost { '*-http':    
+    servername      => 'devops.puppetagent*.com',    
+    port            => '80',    
+    docroot         => '/var/www/html/',    
+    redirect_status => 'permanent',    
+    redirect_dest   => 'https://devops.puppetagent*.com/',  
+  }  
+
+  apache::vhost { '*-https':    
+    servername      => 'devops.puppetagent*.com',    
+    port            => '443',    
+    docroot         => '/var/www/html/',    
+    ssl             => true,    
+  }
 }
